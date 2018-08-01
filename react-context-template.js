f3e53694e7ae4076b382778965c87eea6db0535c
Petr Altman <altick87@hotmail.com>
2018-08-01 12:48:09 +0200

    Create react-context-template.js

---
+++ react-context-template.js
@@ -0,0 +1,32 @@
+// @flow
+
+import * as React from 'react';
+
+const InitialContextState = {
+    someValue: 'Hello world!',
+    actions: {
+        someAction: async () => {}
+    }
+}
+
+const { Consumer, Provider: ContextProvider } = React.createContext(InitialContextState);
+
+class Provider extends React.Component {
+
+    someAction = async (param1, param2) => {
+        
+    }
+
+    state = {
+        ... InitialContextState,
+        actions: {
+            someAction: this.someAction
+        }
+    }
+
+    render = () => (
+        <ContextProvider value={this.state}>{ this.props.children }</ContextProvider>
+    )
+}
+
+export default { Consumer, Provider };
